3ca4467e76cfb82ae1d3d131e54c0ff9252b6dc6
kurtjx <kurtjx@white-top-one.local>
2010-09-28 08:52:22 -0500

    threading close to working

--- echotube/youtube_threaded_calls.py
+++ echotube/youtube_threaded_calls.py
@@ -14,6 +14,20 @@ class ThreadTube(threading.Thread):
 
     def run(self):
         while True:
-            query = self.queue.get()
-
+            idx, query = self.queue.get()
             results = youtube_calls.search(query)
+			output.update({idx:results})
+
+def search(query_list):
+	'''does a threaded call to gdata.youtube for each query in query_list'''
+	# empty output
+	output = {}
+	threads = ThreadTube(queue)
+	# populate the queue
+	for idx, query in query_list:
+		queue.put((idx, query))
+	# run it
+	threads.run()
+	return [result for result in output.values()]
+	
+	
